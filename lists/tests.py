4e79b04cc2893edd65889c0042e0e9c489809fae
Johan Warlander <johan@snowflake.nu>
2014-04-14 22:08:01 +0200

    Make full use of lists to allow multiuser access

--- lists/tests.py
+++ lists/tests.py
@@ -86,3 +86,36 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'], correct_list)
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existin_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+                '/lists/%d/new_item' % (correct_list.id,),
+                data={'item_text': 'A new list item'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+                '/lists/%d/new_item' % (correct_list.id,),
+                data={'item_text': 'A new list item'}
+        )
+
+        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
