a3508d10f5a496ae4c744ad670591fa4e2cdc17d
Johan Warlander <johan@snowflake.nu>
2014-04-16 16:46:26 +0200

    Refactor item saving to use form.save()

--- TODO.md
+++ TODO.md
@@ -1,9 +1,10 @@
 ## TODO
 
--   Support more than one list!
+-   Make form.save() create a list if not given as arg?
 
 ## Done
 
+-   Support more than one list!
 -   Display multiple items in the table
 -   Code smell: POST test is too long?
 -   Don’t save blank items for every request
--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from lists.forms import EMPTY_LIST_ERROR, ItemForm
+from lists.models import List, Item
 
 class ItemFormTest(TestCase):
 
@@ -9,8 +10,17 @@ class ItemFormTest(TestCase):
         self.assertIn('class="form-control input-lg"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
+        list_ = List.objects.create()
         form = ItemForm(data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [EMPTY_LIST_ERROR])
         with self.assertRaises(ValueError):
-            form.save()
+            form.save(for_list=list_)
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()
+        form = ItemForm(data={'text': 'do me'})
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
