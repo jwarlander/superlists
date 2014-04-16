bc4cd3479ce62d45e483feb40d34da98ced53bd1
Johan Warlander <johan@snowflake.nu>
2014-04-16 13:03:01 +0200

    Use get_absolute_url() magic to avoid hard-coded URLs in redirect()

--- lists/views.py
+++ lists/views.py
@@ -14,7 +14,7 @@ def view_list(request, list_id):
         try:
             item.full_clean()
             item.save()
-            return redirect('/lists/%d/' % (list_.id,))
+            return redirect(list_)
         except:
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
@@ -29,4 +29,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {'error': error})
-    return redirect('/lists/%d/' % (list_.id,))
+    return redirect(list_)
