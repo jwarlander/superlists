044530e4a268a3862787deec99fa24a039c1b52c
Johan Warlander <johan@snowflake.nu>
2014-04-17 07:07:52 +0200

    Remove deprecated test + move helper function

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -61,6 +61,17 @@ class NewListTest(TestCase):
 
 class ListViewTest(TestCase):
 
+    # Helper functions
+
+    def post_invalid_input(self):
+        list_ = List.objects.create()
+        return self.client.post(
+                '/lists/%d/' % (list_.id,),
+                data={'text': ''}
+        )
+
+    # Tests
+
     def test_uses_list_template(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
@@ -111,24 +122,6 @@ class ListViewTest(TestCase):
         )
 
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
-
-    def test_validation_errors_end_up_on_lists_page(self):
-        list_ = List.objects.create()
-        response = self.client.post(
-                '/lists/%d/' % (list_.id,),
-                data={'text': ''}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'list.html')
-        expected_error = escape("You can't have an empty list item")
-        self.assertContains(response, expected_error)
-
-    def post_invalid_input(self):
-        list_ = List.objects.create()
-        return self.client.post(
-                '/lists/%d/' % (list_.id,),
-                data={'text': ''}
-        )
 
     def test_for_invalid_input_nothing_saved_to_db(self):
         self.post_invalid_input()
