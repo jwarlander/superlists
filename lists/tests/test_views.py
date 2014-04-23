2e7fc01280aba65b9060b518c1f9aa21a6a62623
Johan Warlander <johan@snowflake.nu>
2014-04-23 13:49:31 +0200

    Real FT for 'My Lists' + initial view and restructured templates

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -165,3 +165,16 @@ class ListViewTest(TestCase):
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
+
+
+class MyListsTest(TestCase):
+
+    def test_my_lists_url_renders_my_lists_template(self):
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertTemplateUsed(response, 'my_lists.html')
+
+    def test_passes_correct_owner_to_template(self):
+        User.objects.create(email='wrong@owner.com')
+        correct_user = User.objects.create(email='a@b.com')
+        response = self.client.get('/lists/users/a@b.com/')
+        self.assertEqual(response.context['owner'], correct_user)
