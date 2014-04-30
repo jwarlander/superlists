443a5ddf2d3b57628e4fa0cd9bebd148799e96b3
Johan Warlander <johan@snowflake.nu>
2014-04-30 18:41:45 +0200

    Clean up some old tests, and implementation code

--- lists/forms.py
+++ lists/forms.py
@@ -20,9 +20,6 @@ class ItemForm(forms.models.ModelForm):
             'text': {'required': EMPTY_LIST_ERROR},
         }
 
-    def save(self, for_list):
-        self.instance.list = for_list
-        return super().save()
 
 class ExistingListItemForm(ItemForm):
 
@@ -36,9 +33,6 @@ class ExistingListItemForm(ItemForm):
         except ValidationError as e:
             e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
             self._update_errors(e)
-
-    def save(self):
-        return forms.models.ModelForm.save(self)
 
 
 class NewListForm(ItemForm):
